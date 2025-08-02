77fa396971964d0fb7f9159fe3d16a0aee978292
Muhammad Imran <imran271828@gmail.com>
2025-08-02 19:52:27 +0600

    chanegd for vercel hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,46 @@
+
+
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const isExport = process.env.EXPORT === 'true';
+
+// Dynamically import the correct adapter
+const adapter = isExport
+  ? (await import('@sveltejs/adapter-static')).default
+  : (await import('@sveltejs/adapter-vercel')).default;
+
+const config = {
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+    paths: {
+      base: isExport ? '/my-subfolder-name' : ''
+    },
+    trailingSlash: 'always'
+  }
+};
+
+export default config;
+
+
+
+
+
+
+
+
+
+
+
+/* uncomment if you want to use the static adapter for exporting 
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config} * /
 const config = {
   kit: {
     adapter: adapter({
@@ -9,9 +49,10 @@ const config = {
       fallback: null
     }),
     paths: {
-      base: ''  // subfolder
+      base: 'suv'  // subfolder
     }
   }
 };
 
 export default config;
+*/
