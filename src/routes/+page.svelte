c4d02a9679437d65cd7e44e11634d8e306e9af01
Muhammad Imran <imran271828@gmail.com>
2025-08-12 16:40:17 +0600

    gogole tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -150,7 +150,18 @@
       }
     }
   </script>
-</svelte:head>
+
+        <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-308EDZQC5K"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-308EDZQC5K');
+      </script>
+
+      </svelte:head>
 
 <style>
   /* --- Progress Bar Styles --- */
